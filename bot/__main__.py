906451ee763e91830f32b65f63cc6466f077617c
prono69 <faltu9851@gmail.com>
2021-07-26 14:04:05 +0530

    kang

--- bot/__main__.py
+++ bot/__main__.py
@@ -34,16 +34,21 @@ def stats(update, context):
     cpuUsage = psutil.cpu_percent(interval=0.5)
     memory = psutil.virtual_memory().percent
     disk = psutil.disk_usage('/').percent
-    stats = f'<b>Bot Uptime:</b> {currentTime}\n' \
-            f'<b>Start Time:</b> <i>{current}</i>\n\n' \
-            f'📀 Storage 📀\n<b>Total Disk Space:</b> {total}\n' \
-            f'<b>Used:</b> {used}  ' \
-            f'<b>Free:</b> {free}\n\n' \
-            f'📊 Data Usage 📊\n<b>Upload:</b> {sent}\n' \
-            f'<b>Down:</b> {recv}\n\n' \
-            f'<b>CPU:</b> {cpuUsage}%\n' \
-            f'<b>RAM:</b> {memory}%\n' \
-            f'<b>DISK:</b> {disk}%'
+    stats = f'<b>╭──「  ⭕️ BOT STATS ⭕️ 」 </b>\n' \
+            f'<b>│</b>\n' \
+            f'<b>├  ⏰ Bot Uptime : {currentTime}</b>\n' \
+            f'<b>├  🔊 Start Time :</b> {current}\n' \
+            f'<b>├  🗄 Total Disk Space : {total}</b>\n' \
+            f'<b>├  🗂 Total Used Space : {used}</b>\n' \
+            f'<b>├  📂 Total Free Space : {free}</b>\n' \
+            f'<b>├  📑 Data Usage :</b>\n' \
+            f'<b>├  📥 Total Download : {recv}</b>\n' \
+            f'<b>├  📤 Total Upload : {sent}</b>\n' \
+            f'<b>├  🖥️ CPU : {cpuUsage}%</b>\n' \
+            f'<b>├  🚀 RAM : {memory}%</b>\n' \
+            f'<b>├  🗄 DISK : {disk}%</b>\n' \
+            f'<b>│</b>\n' \
+            f'<b>╰──「 🚸 @NekoDrive 🚸 」</b>'
     update.effective_message.reply_photo(IMG, stats, parse_mode=ParseMode.HTML)
 
 
